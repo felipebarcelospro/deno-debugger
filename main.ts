d7b490c41cfbc2915394a0ad7767d1b1358751e2
Felipe Barcelos <felipebarcelospro@MacBook-Pro-de-Felipe.local>
2024-07-23 03:09:59 -0300

    feat: update

--- main.ts
+++ main.ts
@@ -1,11 +1,22 @@
-Deno.serve((req) => {
-  const url = new URL(req.url);
-  console.log(`(${req.method}) ${url.pathname} - ${req.headers.get("user-agent")}`);
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-  return new Response("Hello, world", {
-    status: 200,
-    headers: {
-      "content-type": "text/plain; charset=utf-8",
-    },
+const app = new Application();
+const router = new Router();
+
+router
+  .get("/", (context) => {
+    context.response.status = 200;
+    context.response.headers.set("content-type", "text/plain; charset=utf-8");
+    context.response.body = "Hello, world";
+  })
+  .get("/ping", (context) => {
+    context.response.status = 200;
+    context.response.headers.set("content-type", "application/json; charset=utf-8");
+    context.response.body = { message: "pong" };
   });
-});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Server is running on http://localhost:8000");
+await app.listen({ port: 8000 });
